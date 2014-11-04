09c467b83b4b1a0ecb34207838ddde921199e449
Mati Kochen <mati@timble.net>
2014-11-04 13:28:50 +0100

    re #122 - added bootstrapper

---
+++ site/components/com_tada/resources/config/bootstrapper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    Tada
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+
+return array(
+
+    'aliases'    => array(
+        'com://site/tada.database.table.items'     	  => 'com://admin/tada.database.table.items',
+    ),
+);
