cfe151cc7eaec43812b7bf620a8945ee6ce6f5f9
Arunas Mazeika <arunas@timble.net>
2014-11-14 14:07:28 +0100

    re #3 Added activities JSON view.

---
+++ administrator/components/com_todo/views/activities/json.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Todo
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+
+
+class ComTodoViewActivitiesJson extends ComActivitiesViewActivitiesJson
+{
+}
