2251f54cee839856e8291f1316ac2e599bc853c5
Cameron Barr <cameron@timble.com>
2015-05-29 11:54:50 -0400

    #18 - add 'backbone' view and controller

---
+++ site/components/com_todo/controller/backbone.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Todo - a Joomla example extension built with Nooku Framework.
+ *
+ * @package     Todo
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/nooku/joomla-todo for the canonical source repository
+ */
+class ComTodoControllerBackbone extends KControllerView {}
