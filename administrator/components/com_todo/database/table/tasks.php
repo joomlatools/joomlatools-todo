e39ce6d5b28906a02f063314aecfaf4b9e108cfb
Ercan Ozkaya <ercan@timble.net>
2015-07-10 11:40:20 +0300

    Move repository into Joomlatools organization

--- administrator/components/com_todo/database/table/tasks.php
+++ administrator/components/com_todo/database/table/tasks.php
@@ -5,7 +5,7 @@
  * @package     Todo
  * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        https://github.com/nooku/joomla-todo for the canonical source repository
+ * @link        https://github.com/joomlatools/joomlatools-todo for the canonical source repository
  */
 
 class ComTodoDatabaseTableTasks extends KDatabaseTableAbstract
