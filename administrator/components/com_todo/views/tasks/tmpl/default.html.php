4050bb6aa846016d73d64a734ca5a53ea66bd251
Robin Poort <robin@timble.net>
2015-07-16 15:25:16 +0200

    re #23 - chaning markup

--- administrator/components/com_todo/views/tasks/tmpl/default.html.php
+++ administrator/components/com_todo/views/tasks/tmpl/default.html.php
@@ -14,92 +14,153 @@ defined('KOOWA') or die; ?>
 <?= helper('behavior.koowa'); ?>
 
 <ktml:style src="assets://css/admin-joomla.css" />
-<ktml:script src="assets://js/scripts.js" />
+<ktml:script src="assets://js/scriptszzz.js" />
 
 <ktml:module position="toolbar">
     <ktml:toolbar type="actionbar" title="COM_TODO_SUBMENU_TASKS" icon="task icon-stack">
 </ktml:module>
 
+<?php // JFactory::getApplication()->enqueueMessage('Message'); ?>
+
+<script data-inline type="text/javascript">var el = document.body; var cl = 'k-js-enabled'; if (el.classList) { el.classList.add(cl); }else{ el.className += ' ' + cl;}</script>
+
 <!-- Begin List layout -->
 <div class="k-overview k-flex">
-    <form action="" method="get" class="-koowa-grid">
 
-        <!-- The content -->
-        <div class="k-content">
+    <!-- The content -->
+    <form action="" method="get" class="k-content-wrapper -koowa-grid">
 
-            <!-- Sidebar -->
-            <div class="k-sidebar">
+        <!-- Sidebar -->
+        <div class="k-sidebar">
 
-                <!-- Main component navigation -->
-                <div class="k-sidebar__navigation">
-                    <ul class="k-navigation">
+            <!-- Main component navigation -->
+            <div class="k-sidebar__navigation">
+                <ul class="k-navigation">
+                    <li class="active">
+                        <a href="/administrator/index.php?option=com_todo&view=tasks">
+                            <span class="k-icon-home"></span>
+                            <span class="k-title collapsible">Joomla</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/administrator/index.php?option=com_todo&view=tasks&tmpl=component">
+                            <span class="k-icon-grid"></span>
+                            <span class="k-title collapsible">Standalone</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Categories -->
+            <div class="k-sidebar__item">
+                <div class="k-sidebar__content">
+
+                    <ul class="k-list">
+                        <li class="k-tree">
+                            <a href="#">
+                                <span class="k-clicker collapsible"></span>
+                                <span class="k-icon-folder"></span>
+                                <span class="k-title collapsible">All categories</span>
+                            </a>
+                            <ul class="k-tree__items">
+                                <li class="k-list__item--active"><a href="#">Category</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="k-icon-user"></span>
+                                <span class="k-title collapsible">My documents</span>
+                            </a>
+                        </li>
                         <li class="active">
-                            <a href="form.html">
-                                <span class="k-icon-home"></span>
-                                <span class="k-title collapsible">Todos</span>
+                            <a href="#">
+                                <span class="k-icon-clock"></span>
+                                <span class="k-title collapsible">Recently edited</span>
                             </a>
                         </li>
                         <li>
-                            <a href="form.html">
-                                <span class="k-icon-grid"></span>
-                                <span class="k-title collapsible">Activities</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="form.html">
-                                <span class="k-icon-settings"></span>
-                                <span class="k-title collapsible">Categories</span>
+                            <a href="#">
+                                <span class="k-icon-star"></span>
+                                <span class="k-title collapsible">Most popular</span>
                             </a>
                         </li>
                     </ul>
                 </div>
+            </div>
 
-                <!-- Categories -->
-                <div class="k-sidebar__item">
-                    <div class="k-sidebar__content">
+            <!-- Sidebar footer -->
+            <div class="k-sidebar__footer collapsible">
+                © 2015 - Timble
+            </div>
 
-                        <ul class="k-list">
-                            <li class="k-tree">
-                                <a href="#">
-                                    <span class="k-clicker collapsible"></span>
-                                    <span class="k-icon-folder"></span>
-                                    <span class="k-title collapsible">All categories</span>
+        </div> <!-- .k-sidebar -->
+
+        <!-- The content -->
+        <div class="k-content">
+
+            <!-- Toolbar -->
+            <div class="k-toolbar">
+
+                <!-- Sidebar toggle button -->
+                <button class="k-button--toggle k-icon-burger">
+                    <span class="visually-hidden-icon-label">Toggle</span>
+                </button>
+
+                <div class="k-toolbar__buttons">
+                    <a class="btn btn-sm btn-success" href="form.html">New</a>
+                    <button class="btn btn-sm btn-default" disabled>Delete</button>
+                    <button class="btn btn-sm btn-default" disabled>Publish</button>
+                    <button class="btn btn-sm btn-default" disabled>Unpublish</button>
+                    <button class="btn btn-sm btn-default" disabled>Move</button>
+                    <button class="btn btn-sm btn-default btn-right k-modal mfp-iframe" data-mfp-src="select-file.html">Options</button>
+                </div>
+            </div>
+
+            <!-- Component -->
+            <div class="k-component">
+
+                <!-- location bar -->
+                <div class="k-location">
+
+                    <!-- Breadcrumbs -->
+                    <div class="k-breadcrumb">
+                        <ul>
+                            <li class="home">
+                                <a class="k-breadcrumb__item k-icon-home" href="#">
+                                    <span class="visually-hidden-icon-label">Home</span>
                                 </a>
-                                <ul class="k-tree__items">
-                                    <li class="k-list__item--active"><a href="#">Category</a></li>
-                                </ul>
                             </li>
                             <li>
-                                <a href="#">
-                                    <span class="k-icon-user"></span>
-                                    <span class="k-title collapsible">My documents</span>
+                                <a class="k-breadcrumb__item" href="#" data-title="Category 1">
+                                    <span>Category 1</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="k-breadcrumb__item" href="#" data-title="Sub Category 2">
+                                    <span>Sub Category 2</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="k-breadcrumb__item" href="#" data-title="Sub Category 3">
+                                    <span>Sub Category 3</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="k-breadcrumb__item" href="#" data-title="A probably German ridiculous long category name">
+                                    <span>A probably German ridiculous long category name</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="k-breadcrumb__item" href="#" data-title="Category 5">
+                                    <span>Category 5</span>
                                 </a>
                             </li>
                             <li class="active">
-                                <a href="#">
-                                    <span class="k-icon-clock"></span>
-                                    <span class="k-title collapsible">Recently edited</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <span class="k-icon-star"></span>
-                                    <span class="k-title collapsible">Most popular</span>
-                                </a>
+                                <span class="k-breadcrumb__item" data-title="Category 6">Category 6</span>
                             </li>
                         </ul>
                     </div>
                 </div>
-
-                <!-- Sidebar footer -->
-                <div class="k-sidebar__footer collapsible">
-                    © 2015 - Timble
-                </div>
-
-            </div> <!-- .k-sidebar -->
-
-            <!-- Component -->
-            <div class="k-component">
 
                 <div class="k-scopebar">
                     <div class="scopebar-group hidden-tablet hidden-phone">
@@ -123,8 +184,8 @@ defined('KOOWA') or die; ?>
                     </div>
                 </div>
 
-                <div class="todo_table_container">
-                    <table class="table table-striped footable">
+                <div class="k-table">
+                    <table>
                         <thead>
                             <tr>
                                 <th style="text-align: center;" width="1">
@@ -185,7 +246,9 @@ defined('KOOWA') or die; ?>
                         </tbody>
                     </table>
                 </div>
-            </div>
-        </div>
-    </form>
+            </div><!-- .k-component -->
+        </div><!-- k-content -->
+
+    </form><!-- .k-content-wrapper -->
+
 </div><!-- .k-overview -->
