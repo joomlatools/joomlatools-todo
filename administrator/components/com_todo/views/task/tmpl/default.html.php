adf67f0b01ac6aa45fb610e5e05a201154f9d819
Robin Poort <robin@timble.net>
2015-08-04 09:57:02 +0200

    re #23 - removing bootstrap grid

--- administrator/components/com_todo/views/task/tmpl/default.html.php
+++ administrator/components/com_todo/views/task/tmpl/default.html.php
@@ -54,140 +54,129 @@ defined('KOOWA') or die; ?>
             <div class="k-component">
 
                 <!-- Container -->
-                <div class="container-fluid">
+                <div class="k-container">
 
-                    <!-- Row -->
-                    <div class="row">
+                    <!-- Main information -->
+                    <div class="k-container__main">
 
-                        <!-- Two thirds -->
-                        <div class="col-sm-8">
+                        <fieldset>
 
-                            <fieldset>
+                            <?php // @TODO: make sure we can just delete this <legend> without a new one being created ?>
+                            <legend style="height:0;overflow:hidden;padding:0;margin:0;border:none;"><?= translate('') ?></legend>
 
-                                <?php // @TODO: make sure we can just delete this <legend> without a new one being created ?>
-                                <legend style="height:0;overflow:hidden;padding:0;margin:0;border:none;"><?= translate('') ?></legend>
+                            <div class="control-group">
+                                <div class="controls">
 
-                                <div class="row">
-                                    <div class="control-group col-xs-12">
-                                        <div class="controls">
+                                    <input
+                                        required
+                                        class="form-control input-lg"
+                                        id="todo_form_title"
+                                        type="text"
+                                        name="title"
+                                        maxlength="255"
+                                        value="<?= escape($task->title); ?>"
+                                        placeholder="Enter title here"
+                                    />
 
-                                            <input
-                                                required
-                                                class="form-control input-lg"
-                                                id="todo_form_title"
-                                                type="text"
-                                                name="title"
-                                                maxlength="255"
-                                                value="<?= escape($task->title); ?>"
-                                                placeholder="Enter title here"
-                                            />
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="controls">
 
+                                    <div class="input-group input-group-sm">
+                                        <label for="todo_form_alias" class="input-group-addon">
+                                            Alias
+                                        </label>
+                                        <input
+                                            id="todo_form_alias"
+                                            type="text"
+                                            class="form-control"
+                                            name="slug"
+                                            maxlength="255"
+                                            value="<?= escape($task->slug) ?>"
+                                            placeholder="Will be created automatically"
+                                        />
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-md btn-default mfp-iframe" data-mfp-src="/media/koowa/framework/new-ui/layouts/select-document.php">Select document</button>
+                                        </div>
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-md btn-default mfp-iframe" data-mfp-src="/media/koowa/framework/new-ui/layouts/select-file.php">Select file</button>
                                         </div>
                                     </div>
-                                    <div class="control-group col-xs-12">
-                                        <div class="controls">
+                                </div>
+                            </div>
 
-                                            <div class="input-group input-group-sm">
-                                                <label for="todo_form_alias" class="input-group-addon">
-                                                    Alias
-                                                </label>
-                                                <input
-                                                    id="todo_form_alias"
-                                                    type="text"
-                                                    class="form-control"
-                                                    name="slug"
-                                                    maxlength="255"
-                                                    value="<?= escape($task->slug) ?>"
-                                                    placeholder="Will be created automatically"
-                                                />
-                                                <div class="input-group-btn">
-                                                    <button class="btn btn-md btn-default mfp-iframe" data-mfp-src="/media/koowa/framework/new-ui/layouts/select-document.php">Select document</button>
+                            <div class="control-group">
+                                <div class="controls">
+                                    <?= helper('editor.display', array(
+                                        'name' => 'description',
+                                        'value' => $task->description,
+                                        'id'   => 'description',
+                                        'width' => '100%',
+                                        'height' => '341',
+                                        'cols' => '100',
+                                        'rows' => '20',
+                                        'buttons' => array('pagebreak')
+                                    )); ?>
+                                </div>
+                            </div>
+
+                        </fieldset>
+
+                    </div><!-- .k-container__main -->
+
+                    <!-- Other information -->
+                    <div class="k-container__sub">
+
+                        <fieldset class="k-form-block">
+
+                            <div class="k-form-block__header">
+                                <?= translate('Publishing') ?>
+                            </div>
+
+                            <div class="k-form-block__content">
+                                <div class="control-group">
+                                    <div class="control-content">
+                                        <label class="control-label">Status</label>
+                                        <div class="controls">
+                                            <div class="radio-toggle">
+                                                <div class="radio-toggle-item">
+                                                    <input type="radio" name="status" id="status1" value="1" checked="checked">
+                                                    <label for="status1">
+                                                        <span>
+                                                            Published
+                                                        </span>
+                                                    </label>
                                                 </div>
-                                                <div class="input-group-btn">
-                                                    <button class="btn btn-md btn-default mfp-iframe" data-mfp-src="/media/koowa/framework/new-ui/layouts/select-file.php">Select file</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="control-group col-xs-12">
-                                        <div class="controls">
-                                            <?= helper('editor.display', array(
-                                                'name' => 'description',
-                                                'value' => $task->description,
-                                                'id'   => 'description',
-                                                'width' => '100%',
-                                                'height' => '341',
-                                                'cols' => '100',
-                                                'rows' => '20',
-                                                'buttons' => array('pagebreak')
-                                            )); ?>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </fieldset>
-
-                        </div><!-- Two thirds -->
-
-                        <!-- One third -->
-                        <div class="col-sm-4">
-
-                            <fieldset class="k-form-block">
-
-                                <div class="k-form-block__header">
-                                    <?= translate('Publishing') ?>
-                                </div>
-
-                                <div class="k-form-block__content">
-                                    <div class="row">
-                                        <div class="control-group col-xs-12">
-                                            <div class="control-content">
-                                                <label class="control-label">Status</label>
-                                                <div class="controls">
-                                                    <div class="radio-toggle">
-                                                        <div class="radio-toggle-item">
-                                                            <input type="radio" name="status" id="status1" value="1" checked="checked">
-                                                            <label for="status1">
-                                                                <span>
-                                                                    Published
-                                                                </span>
-                                                            </label>
-                                                        </div>
-                                                        <div class="radio-toggle-item">
-                                                            <input type="radio" name="status" id="status0" value="0">
-                                                            <label for="status0">
-                                                                <span>
-                                                                    Unpublished
-                                                                </span>
-                                                            </label>
-                                                        </div>
-                                                    </div>
+                                                <div class="radio-toggle-item">
+                                                    <input type="radio" name="status" id="status0" value="0">
+                                                    <label for="status0">
+                                                        <span>
+                                                            Unpublished
+                                                        </span>
+                                                    </label>
                                                 </div>
                                             </div>
-
-                                            <?php // @TODO: Make sure code below generates code above
-                                            if ( 1 == 2 ) :?>
-                                                <?= helper('select.booleanlist', array(
-                                                    'name' => 'enabled',
-                                                    'selected' => $task->enabled,
-                                                    'true' => translate('Published'),
-                                                    'false' => translate('Unpublished')
-                                                )); ?>
-                                            <?php endif; ?>
-
                                         </div>
                                     </div>
+
+                                    <?php // @TODO: Make sure code below generates code above
+                                    if ( 1 == 2 ) :?>
+                                        <?= helper('select.booleanlist', array(
+                                            'name' => 'enabled',
+                                            'selected' => $task->enabled,
+                                            'true' => translate('Published'),
+                                            'false' => translate('Unpublished')
+                                        )); ?>
+                                    <?php endif; ?>
+
                                 </div>
-                            </fieldset>
+                            </div>
+                        </fieldset>
 
-                        </div><!-- One third -->
+                    </div><!-- .k-container__sub -->
 
-                    </div><!--.row -->
-
-                </div><!-- .container-fluid -->
+                </div><!-- .k-container -->
 
             </div><!-- .k-component -->
 
