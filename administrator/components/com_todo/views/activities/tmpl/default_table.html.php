c8d115de0b1789548207fadbdf0b0b1a9ee54a98
Robin Poort <robin@timble.net>
2015-10-28 16:43:05 +0100

    re #23 - adding missing copyright info

--- administrator/components/com_todo/views/activities/tmpl/default_table.html.php
+++ administrator/components/com_todo/views/activities/tmpl/default_table.html.php
@@ -1,3 +1,15 @@
+<?
+/**
+ * Todo - a Joomla example extension built with Nooku Framework.
+ *
+ * @package     Todo
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-todo for the canonical source repository
+ */
+
+defined('KOOWA') or die; ?>
+
 <div class="k-table-container">
     <div class="k-table">
         <table class="table--fixed">
--- administrator/components/com_todo/views/tasks/tmpl/default_sidebar.html.php
+++ administrator/components/com_todo/views/tasks/tmpl/default_sidebar.html.php
@@ -1,3 +1,15 @@
+<?
+/**
+ * Todo - a Joomla example extension built with Nooku Framework.
+ *
+ * @package     Todo
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-todo for the canonical source repository
+ */
+
+defined('KOOWA') or die; ?>
+
 <div id="k-sidebar" class="k-sidebar">
     <!-- Navigation -->
     <div class="k-sidebar__navigation">
