dbe32b59213cdf123d8c7ae7a1d0b33debe19b08
Rastin Mehr <rastin@timble.net>
2015-10-30 02:02:36 +0000

    issue #7 creating tag tables and initiating this branch

--- administrator/components/com_todo/resources/install/install.sql
+++ administrator/components/com_todo/resources/install/install.sql
@@ -48,6 +48,47 @@ CREATE TABLE IF NOT EXISTS `#__activities` (
     KEY `ip` (`ip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__todo_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `#__todo_tags` (
+  `tags_tag_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `table` varchar(50) NOT NULL,
+  `created_by` int(10) unsigned DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `modified_by` int(10) unsigned DEFAULT NULL,
+  `modified_on` datetime DEFAULT NULL,
+  `locked_by` int(10) unsigned DEFAULT NULL,
+  `locked_on` datetime DEFAULT NULL,
+  `params` text NOT NULL,
+  `uuid` char(36) NOT NULL,
+  PRIMARY KEY (`tags_tag_id`),
+  UNIQUE KEY `slug` (`slug`),
+  UNIQUE KEY `title` (`title`),
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `table` (`table`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__todo_tags_relations`
+--
+
+CREATE TABLE IF NOT EXISTS `#__todo_tags_relations` (
+	`tags_tag_id` BIGINT(20) UNSIGNED NOT NULL,
+  `row` BIGINT(20) UNSIGNED NOT NULL,
+  `table` VARCHAR( 255 ) NOT NULL,
+  PRIMARY KEY  (`tags_tag_id`,`row`,`table`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 --
 -- Dumping data for table `#__activities_activities`
 --
