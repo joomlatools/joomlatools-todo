e3a6914f8c7067a135a275dd074f4b68dbf02850
Arunas Mazeika <arunas@timble.net>
2015-12-04 12:21:15 +0100

    re #83 Updated install dump file with latest attachments DB schemas.

--- administrator/components/com_todo/resources/install/install.sql
+++ administrator/components/com_todo/resources/install/install.sql
@@ -81,17 +81,31 @@ CREATE TABLE IF NOT EXISTS `#__files_thumbnails` (
   PRIMARY KEY (`files_thumbnail_id`)
 ) DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `#__files_attachments` (
+CREATE TABLE `#__files_attachments` (
   `files_attachment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `files_container_id` bigint(20) NOT NULL,
   `uuid` char(36) NOT NULL DEFAULT '',
-  `table` varchar(255) NOT NULL DEFAULT '',
-  `row` bigint(20) unsigned NOT NULL,
+  `path` varchar(2000) NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
+  `description` text,
+  `created_by` bigint(20) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `modified_by` bigint(20) NOT NULL,
+  `modified_on` datetime NOT NULL,
+  `locked_by` bigint(20) NOT NULL,
+  `locked_on` datetime NOT NULL,
   PRIMARY KEY (`files_attachment_id`),
   UNIQUE KEY `uuid` (`uuid`),
-  UNIQUE KEY `table` (`table`,`row`,`name`)
+  UNIQUE KEY `files_container_id` (`files_container_id`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `#__files_attachments_relations` (
+  `files_attachment_id` bigint(20) NOT NULL,
+  `table` varchar(255) NOT NULL DEFAULT '',
+  `row` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`files_attachment_id`,`table`,`row`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 INSERT INTO `#__files_containers` (`files_container_id`, `slug`, `title`, `path`, `parameters`)
 VALUES
