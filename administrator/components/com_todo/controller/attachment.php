8d940da60b1715dcf32c0f141187613d268a4647
Arunas Mazeika <arunas@timble.net>
2015-12-04 12:22:19 +0100

    #9 Added attachment controller.

---
+++ administrator/components/com_todo/controller/attachment.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Todo - a Joomla example extension built with Nooku Framework.
+ *
+ * @package     Todo
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/nooku/joomla-todo for the canonical source repository
+ */
+
+/**
+ * Attachment Controller
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Nooku\Component\Todo
+ */
+class ComTodoControllerAttachment extends ComFilesControllerAttachment
+{
+}
